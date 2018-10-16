d603c30724f0db223ed43afa31b46a9e0b60d8e6
nkmuzyka <44164760+nkmuzyka@users.noreply.github.com>
2018-10-16 11:18:36 +0300

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,19 +1,47 @@
-open.account <- function(total) {
-	list(
-		deposit = function(amount) {
-			if(amount <= 0)
-				stop("You have negative deposits!\n")
-		total <<- total + amount
-		cat(amount, "deposited. Your balance is", total, "\n\n")
-		},
-		withdraw = function(amount) {
-			if(amount > total)
-				stop("You are out of money, bro!\n")
-		total <<- total - amount
-		cat(amount, "withdrawn. Your balance is", total, "\n\n")
-		},
-		balance = function() {
-			cat("Your balance is", total, "\n\n")
-		}
+## The functions reperesented below do the caching and
+## inverse of a generic matrix.
+
+## Function is responsible for object creation that is capable
+## of caching matrix object and its inverse.
+
+makeCacheMatrix <- function(mx = matrix()) {
+	imx <- NULL
+	
+	get <- function() return(mx)
+	
+	getInverse <- function() return(imx)
+	
+	set <- function(val) {
+		mx <<- val
+		inverse <<- NULL
+	}
+	
+	setInverse <- function(val) imx <<- val
+	
+	return(
+		list(
+			set = set,
+			get = get,
+			setInverse = setInverse,
+			getInverse = getInverse
+		)	
 	)
+}
+
+
+## Function gets inverse from cached matrix.
+## In case cached matrix is null null is returned.
+## If inverted matrix is not present in cache, its
+## value is calculated and cached.
+
+cacheSolve <- function(cmx, ...) {
+	imx <- cmx$getInverse()
+	
+	if (!is.null(imx)) return(imx)
+	
+	mx <- cmx$get()
+	imx <- solve(mx, ...)
+	cmx$setInverse(imx)
+	
+	return(imx)
 }
